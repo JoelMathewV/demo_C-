65f7cf4318edd60fd7607f2083a99b808abd05a8
JoelMathewV <joelmathewv0819@gmail.com>
2021-04-07 22:59:17 +0530

    updated bit commands

--- bit.cpp
+++ bit.cpp
@@ -3,7 +3,14 @@ using namespace std;
 int getBit(int n, int poss){
     return ((n & (1<<poss))!=0);
 }
+int setBit(int n, int poss){
+    return(n|(1<<poss));
+}
+int clearBit(int n, int poss){
+    int mask= ~(1<<poss);
+    return (n & mask);
+}
 int main(){
-    cout<<getBit(5,2)<<endl;
+    cout<<clearBit(5,2)<<endl;
     return 0;
 }
