ba0de5c6ccdd716274cce7d7dd0f2dd98f6e3416
JoelMathewV <joelmathewv0819@gmail.com>
2021-04-23 11:40:20 +0530

    function to find the prime factors of a no.

--- sieveoferatosthenes.cpp
+++ sieveoferatosthenes.cpp
@@ -16,10 +16,30 @@ void sieveoferatosthenes(int a){
         }
     } 
 }
+//prime factorisation of a given no.
+void primeFactorisation(int n){
+    int arr[100]={0};
+    for(int i=2;i<=n;i++){
+        arr[i]=i;
+    }
+    for(int i=2;i<=n;i++){
+        if(arr[i]==i){
+            for(int j=i*i;j<=n;j+=i){
+                if(arr[j]==j){
+                    arr[j]=i;
+                }
+            }
+        }
+    }
+    while(n!=1){
+        cout<<arr[n]<<' ';
+        n=n/arr[n];
+    }
+}
 int main(){
     int n;
     cin>>n;
-    sieveoferatosthenes(n);
+    primeFactorisation(n);
     cout<<endl;
     return 0;
 }
