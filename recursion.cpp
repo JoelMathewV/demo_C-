185f7f2f54a3be30959881a0e81b9cec4a5e23b4
JoelMathewV <joelmathewv0819@gmail.com>
2021-04-30 14:41:12 +0530

    fn to check if an array is sorted or not

--- recursion.cpp
+++ recursion.cpp
@@ -32,8 +32,18 @@ int fibbo(int n){
     }
     return fibbo(n-1)+fibbo(n-2);
 }
+//to check if an array is sorted or not
+bool sort(int arr[], int n){
+    if(n==1){
+        return true;
+    }
+    bool restArr = sort(arr+1, n-1);
+    return (arr[0]<arr[1] && restArr);
+}
 
 int main(){
-    cout<<fibbo(4)<<endl;
+    int arr[5] = {1,2,3,4,5};
+    int n=5;
+    cout<<sort(arr, n)<<endl;
     return 0;
 }
