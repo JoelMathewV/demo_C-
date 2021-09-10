2ae4c16bbd97495d575afafc2c36f2b4e14aada4
JoelMathewV <joelmathewv0819@gmail.com>
2021-09-11 00:15:02 +0530

    test

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -1,12 +1,24 @@
 #include<iostream>
 using namespace std;
 class employee{
+    public:
     string name;
     int age;
     string company;
-    
+
+    void Introduce(){
+        cout<<"name = "<<name<< endl;
+        cout<<"age = "<<age<< endl;
+        cout<<"company = "<< company << endl;
+    }
+
 };
 int main(){
+    employee joel;
+    joel.name = "joel";
+    joel.age = 19;
+    joel.company = "google";
+    joel.Introduce();
 
     return 0;
 }
