9703abb5a9a595249033999844a9cc682d1c5657
JoelMathewV <joelmathewv0819@gmail.com>
2021-09-26 18:15:05 +0530

    abstraction

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
-class employee{
+class abstractEmp{                          //abstraction
+    virtual void AskForPromotion()=0;
+};
+class employee:abstractEmp{
 
     string name;
     int age;
@@ -13,10 +16,30 @@ class employee{
     string getName(){
         return name;
     }
+    void setAge(int Age){              //encapsulation
+        age = Age;
+    }
+    int getAge(){
+        return age;
+    }
+    void setCompany(string Company){              //encapsulation
+        company = Company;
+    }
+    string getCompany(){
+        return company;
+    }
     void Introduce(){
         cout<<"name = "<<name<< endl;
         cout<<"age = "<<age<< endl;
         cout<<"company = "<< company << endl;
+    }
+    void AskForPromotion(){                     //abstraction
+        if(age>20){
+            cout<<"u r promoted"<<endl;
+        }
+        else{
+            cout<<"sorry u r not promoted"<<endl;
+        }
     }
 
     employee(string Name, string Company, int Age){         //constructor
@@ -30,6 +53,6 @@ int main(){
     employee joel = employee("joel", "amazon", 19);
     joel.Introduce();
     joel.setName("noel");
-    cout<<joel.getName()<<endl;
+    joel.AskForPromotion();
     return 0;
 }
