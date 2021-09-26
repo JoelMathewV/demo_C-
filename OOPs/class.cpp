80d4e4bec9b055638a3a72ebea8edfd17d0d6d35
JoelMathewV <joelmathewv0819@gmail.com>
2021-09-26 16:37:00 +0530

    learned to use constructor

--- OOPs/class.cpp
+++ OOPs/class.cpp
@@ -12,12 +12,18 @@ class employee{
         cout<<"company = "<< company << endl;
     }
 
+    employee(string Name, string Company, int Age){
+        name = Name;
+        company = Company;
+        age = Age;
+    }
+
 };
 int main(){
-    employee joel;
-    joel.name = "joel";
-    joel.age = 19;
-    joel.company = "google";
+    employee joel = employee("joel", "amazon", 19);
+    //joel.name = "joel";
+    //joel.age = 19;
+    //joel.company = "google";
     joel.Introduce();
 
     return 0;
